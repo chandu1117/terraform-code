7eedecf51c6a2cf08472c061b8383ae09d8eae40
root <root@ip-172-31-3-28.ap-south-1.compute.internal>
2022-12-17 17:45:26 +0000

    msg

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIAUWQYJFPTBDJ65WPH"
+secret_key = "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"
+}
---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+default = "ami-0cca134ec43cf708f"
+}
+variable "instance_type" {
+default = "t2.micro"
+}
+variable "key_name" {
+default = "projectkeypair"
+}
+variable "name" {
+default = "my-instance"
+}
